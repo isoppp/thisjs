c756afdf3ddd7b4b92456314400ff7d6768d2bc6
isoppp <prog@d2deck.com>
2016-04-05 11:57:38 +0900

    cssをsass化

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
+var sass = require('gulp-ruby-sass');
 
 gulp.task('concat', function() {
   return gulp.src('./md/*.md')
@@ -7,6 +8,12 @@ gulp.task('concat', function() {
     .pipe(gulp.dest('./md_concat/'));
 });
 
+gulp.task('sass', function() {
+  return sass('./scss/index.scss', { style: 'expanded' })
+    .pipe(gulp.dest('./'));
+});
+
 gulp.task('watch', function(){
   gulp.watch('./md/*.md', ['concat']);
+  gulp.watch('./scss/*.scss', ['sass']);
 });
